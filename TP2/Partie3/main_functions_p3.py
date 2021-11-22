6bb8e6df92ce5611ebdf1f11f659e6f7181a2a76
Erwanlbv <er.leblevec@gmail.com>
2021-11-23 00:08:38 +0100

    Suppression ligne inutile genere_chaine2

--- TP2/Partie3/main_functions_p3.py
+++ TP2/Partie3/main_functions_p3.py
@@ -5,12 +5,12 @@ from Programmes_Annexes import set_params
 from TP2.Partie2.main_functions_p2 import *
 
 
-def tirage_classe2(p1, cl1, cl2): # Question 1
+def tirage_classe2(p1, cl1, cl2):# Question 1
     a = np.random.rand()
     return cl1*(a <= p1) + cl2*(a > p1)
 
 
-def genere_chaine2(n, params): # Question 2
+def genere_chaine2(n, params):# Question 2
     chain = [tirage_classe2(params['a'], params['cl1'], params['cl2'])]
 
     for i in range(1, n):
@@ -19,7 +19,6 @@ def genere_chaine2(n, params): # Question 2
             (chain[i-1] == params['cl2']) * tirage_classe2(params['A'][1][0], params['cl1'], params['cl2'])
         )
     return np.array(chain)
-
 
 
 
